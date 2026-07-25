e2496c0e7e4768f5533db26ae562d5a6f06c473a
khantphyoemin537-star <khantphyoemin537@gmail.com>
2026-07-25 15:13:28 +0630

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from telethon.tl.types import UserStatusEmpty, UserStatusOffline, UserStatusRecently, UserStatusLastWeek, UserStatusLastMonth
 from telethon.tl.functions.messages import ImportChatInviteRequest, CheckChatInviteRequest
 from telethon.errors import FloodWaitError
-from telethon.errors.rpcerrorlist import UserNotParticipantError, AlreadyParticipantError
-
+from telethon.errors import FloodWaitError
+from telethon.errors import UserNotParticipantError, AlreadyParticipantError
 # ==========================================
 # ⚙️ CONFIGURATION
 # ==========================================
